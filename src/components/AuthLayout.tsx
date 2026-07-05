d2c1b199a4e6542ab48c6ac1e266c87b7b7a2db5
Skip <skip@usecurling.com>
2026-07-05 12:49:49 -0300

    Fix invalid props in Login component

--- src/components/AuthLayout.tsx
+++ src/components/AuthLayout.tsx
@@ -1,5 +1,9 @@
 import { Outlet } from 'react-router-dom'
 
 export default function AuthLayout() {
-  return <Outlet />
+  return (
+    <div className="min-h-screen bg-background">
+      <Outlet />
+    </div>
+  )
 }
