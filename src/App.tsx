e941262c3c78447a24236355daa380f906deb01d
Skip <skip@usecurling.com>
2026-07-06 21:12:19 -0300

    Refactor Login page JSX to resolve fragment error

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route, useLocation, Navigate } from 'react-router-dom'
 import { useEffect, lazy } from 'react'
-import '@/lib/strip-fragment-uid'
 import { Toaster } from '@/components/ui/toaster'
 import { Toaster as Sonner } from '@/components/ui/sonner'
 import { TooltipProvider } from '@/components/ui/tooltip'
